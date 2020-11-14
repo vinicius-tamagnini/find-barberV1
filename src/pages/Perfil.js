6100de7418486144ad65c77f7828b302ce528eed
vinicius-tamagnini <vinitamagnini2@gmail.com>
2020-11-14 00:31:48 -0300

    agora é possivel fazer a leitura dos dados salvos no banco

--- src/pages/Perfil.js
+++ src/pages/Perfil.js
@@ -1,132 +1,184 @@
-import React from 'react';
-import { StyleSheet, View, Image, TouchableOpacity, Text, ScrollView, Button} from 'react-native';
-import firefirebase from '../Connection';
-
-
-
-export default function Perfil() {
-  return(
-    <View style= {styles.view}>
-      <ScrollView style= {styles.scroll}>
-        <Image 
-          style= {styles.logo}
-          source={require('../../assets/icons/usuazin.png')}
-        />
-        <Image 
-          style= {styles.foto}
-          source={require('../../assets/icons/fotin.png')}
-        />
-        <Text style = {styles.texto}> Nome:</Text>
-        <Text>__________________________________________________</Text>
-        <Image style = {styles.caneta1}
-          source={require('../../assets/icons/pencil.png')}
-        />
-        <Text style = {styles.texto}> Email:</Text>
-        <Text>__________________________________________________</Text>
-        <Image style = {styles.caneta2}
-          source={require('../../assets/icons/pencil.png')}
-        />
-        <Text style = {styles.texto}> Data de nascimento:</Text>
-        <Text>__________________________________________________</Text>
-        <Image style = {styles.caneta3}
-          source={require('../../assets/icons/pencil.png')}
-        />
-        <Text style = {styles.texto}> Cartão:</Text>
-        <Text>__________________________________________________</Text>
-        <Image style = {styles.caneta4}
-          source={require('../../assets/icons/pencil.png')}
-        />
-
-        <TouchableOpacity style= {styles.botaoSair}>
-          <Text style= {styles.texto}>SAIR</Text>
-        </TouchableOpacity>
-
-      </ScrollView>
-    </View>
-
-  );
   
+import React, {useState, useEffect} from 'react';
+import { FlatList, StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
+import firebase from '../Connection';
 
+
+
+export default function Perfil(props) {
+
+const [listClientes, setListClientes] = useState([]);
+const [listaDados, setListaDados] = useState([]);
+/*
+useEffect(() => {
+  try {
+    firebase.database().ref('/dadosBabeiro').on('value', (snapshot) => {
+
+      const list = [];
+      
+      snapshot.forEach((childItem) => {
+        list.push({
+          key: childItem.key,
+          email: childItem.val().email,
+          nomeBarbearia: childItem.val().nomeBarbearia,
+        });
+      });
+      setListaDados(list);
+    })
+  } catch (error) {
+    alert(error);
   }
+
+  
+}, [])
+*/
+useEffect(() => {
+  try {
+    firebase.database().ref('/dadosBarbeiro').on('value', (snapshot) => {
+      let list = [];
+      
+      snapshot.forEach((childItem) => {
+        list.push({
+          email: childItem.val().email,
+          nomeBarbearia: childItem.val().nomeBarbearia,
+          endereco: childItem.val().endereco
+          
+        });
+      });
+      setListaDados(list);
+    })
+
+  } catch (error) {
+    alert(error);
+  }
+}, [])
+
+/*
+let total = listClientes.length
+
+for( var i = 0 ; i < total ; i++){
+  if (listClientes[i].email === listEmail[0].email){
+  */
+    return (
+        <View style = {styles.container}>
+            <FlatList
+                data={listaDados}
+                keyExtractor={(item) =>item.key}
+                renderItem={({item}) =>
+                <View>
+                    <Image source={require('../../assets/icons/usuazin.png')} style={styles.usuario}/>
+                    <Image source={require('../../assets/icons/perfilzin.png')} style={styles.profile}/>
+                    <Image source={require('../../assets/icons/emailzin.png')} style={styles.email}/>
+                    <Image source={require('../../assets/icons/calendazin.png')} style={styles.calendar}/>
+                    
+                    
+                    <View style={styles.alltexts}>
+                    <Text style={styles.title}>Nome: </Text>
+                    <Text style={styles.text}>{item.email}</Text>
+                    <Text style={styles.line}>━━━━━━━━━━━━━━━━━━━</Text>
+                    <Text style={styles.title}>Email: </Text>
+                    <Text style={styles.text}>{item.nomeBarbearia}</Text>
+                    <Text style={styles.line}>━━━━━━━━━━━━━━━━━━━</Text>
+                    <Text style={styles.title}>Endereço: </Text>
+                    <Text style={styles.text}>{item.endereco}</Text>
+                    <Text style={styles.line}>━━━━━━━━━━━━━━━━━━━</Text>
+                    
+                    </View>
+                    <TouchableOpacity style = {styles.botao} 
+                    onPress = { () => {props.navigation.navigate('Login')}}
+                    >
+                    <Text style = {styles.botaoText}>Sair</Text>
+                    </TouchableOpacity>
+                
+                </View>
+        }
+            />       
+        </View>
+      
+    );  
+
+  /*}else {
+    console.log("Algo errado aconteceu")
+  }
+}
+  return(
+    <Text>Erro no banco</Text>
+  );
+  */
+}
+
 
 
 const styles = StyleSheet.create({
-  logo: {
-    margin: 0,
-    justifyContent: 'center',
-    width: 185,
-    height: 175,
-    marginTop: 50,
-    marginLeft: 80
+  container: {
+
   },
-  texto: {
-    fontSize: 21,
-    marginTop: 30,
-    marginBottom: 20,
-    fontWeight: "bold",
+  usuario: {
+    width: 120,
+    height: 120,
+    marginTop: 20,
+    alignSelf: "center", 
   },
-  numero: {
+  profile:{
+    width: 30,
+    height: 30,
+    marginLeft: 20,
+    position: "absolute",
+    marginTop: 160,
+  },
+  email:{
+    width: 30,
+    height: 30,
+    marginLeft: 20,
+    position: "absolute",
+    marginTop: 230,
+  },
+  calendar:{
+    width: 30,
+    height: 30,
+    marginLeft: 20,
+    position: "absolute",
+    marginTop: 310,
+  },
+  card:{
+    width: 30,
+    height: 30,
+    marginLeft: 20,
+    position: "absolute",
+    marginTop: 375,
+  },
+  alltexts:{
+    marginTop: 10,
+  },
+  title:{
+    fontWeight: 'bold',
     fontSize: 20,
-    height:25,
-    
+    marginLeft: 60,
   },
-  botaoSair :{
+  text:{
+    fontWeight: 'bold',
+    fontSize: 16,
+    marginLeft: 120,
+  },
+  line:{
+    alignSelf: "center",
+    fontWeight: 'bold',
+    fontSize: 14,
+    marginTop: 7,
+  },
+  botao:{
     width: 200,
-    height: 70,
-    borderRadius: 5,
-    marginTop: 50,
-    
+    height: 50,
+    borderRadius: 10,
+    marginTop: 60,
     backgroundColor: "#012F6B",
     justifyContent: 'center',
     alignItems: "center",
-    marginLeft: 78,
+    alignSelf: "center",
   },
-  botaoTexto:{
+  botaoText :{
     fontSize: 16,
     fontWeight: "bold",
-    color: "white",
-
-  },
-  view: {
-    marginLeft: 50,
-    marginRight: 50
-  },
-  scroll: {
-    marginRight: 0,
-  },
-  foto:{
-    position: "absolute",
-    marginTop: 176,
-    marginLeft: 197,
-    width:48,
-    height: 46,
-  },
-  caneta1:{
-    position: "absolute",
-    marginTop: 255,
-    marginLeft: 250,
-    width: 35,
-    height: 35,
-  },
-  caneta2:{
-    position: "absolute",
-    marginTop: 355,
-    marginLeft: 250,
-    width:35,
-    height: 35,
-  },
-  caneta3:{
-    position: "absolute",
-    marginTop: 455,
-    marginLeft: 250,
-    width:35,
-    height: 35,
-  },
-  caneta4:{
-    position: "absolute",
-    marginTop: 555,
-    marginLeft: 250,
-    width:35,
-    height: 35,
+    color: "#fff",
   },
 });
